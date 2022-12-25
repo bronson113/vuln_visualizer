4ddf36f503ac8ead738004fa38ee263a4ac40cbc
Bronson Yen <bronson1013@gmail.com>
2022-12-24 23:38:11 -0500

    Added simple compile and run functionality

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import os, subprocess
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def index():
+  return render_template('index.html')
+
+def compile_code_os(code_string):
+  with open("/tmp/temp.c", "w") as f:
+    f.write(code_string)
+  os.system("gcc /tmp/temp.c -o /tmp/temp.out")
+  os.system("chmod +x /tmp/temp.out")
+  output = subprocess.check_output("/tmp/temp.out", stderr=subprocess.STDOUT).decode()
+  return output
+
+
+@app.route("/compile", methods = ['POST'])
+def compile_code():
+  input_code = request.form['input_code']
+  try:
+    result = compile_code_os(input_code)
+  except Exception as e:
+    print(e)
+    result = "An error occured during compilation"
+  return render_template('compile.html', result=result)
+
 
 if __name__ == "__main__":
   app.run()
